3c27247f07124228b49097bbacd978f0957a1675
ericchentch <ericchen.tch@gmail.com>
2022-10-26 11:35:41 +0700

    ignore default mongo config

--- src/main/java/com/example/ecom/EComApplication.java
+++ src/main/java/com/example/ecom/EComApplication.java
@@ -2,12 +2,14 @@ package com.example.ecom;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { MongoAutoConfiguration.class, MongoDataAutoConfiguration.class })
 public class EComApplication {
 
 	public static void main(String[] args) {
