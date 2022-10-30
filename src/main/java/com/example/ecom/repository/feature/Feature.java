a3f2f73d5c963d749ae405b4c7d66b7a230732b2
ericchentch <ericchen.tch@gmail.com>
2022-10-30 19:38:24 +0700

    init feature and permissions

--- src/main/java/com/example/ecom/repository/feature/Feature.java
+++ src/main/java/com/example/ecom/repository/feature/Feature.java
@@ -1,11 +1,13 @@
 package com.example.ecom.repository.feature;
 
 import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Document(collection = "features")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
