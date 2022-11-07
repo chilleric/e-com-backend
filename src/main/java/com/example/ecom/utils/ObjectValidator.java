1c3d98ec173be58fa0927c57283c3d1d054ff31c
ericchentch <ericchen.tch@gmail.com>
2022-11-07 17:04:32 +0700

    config fly.io

--- src/main/java/com/example/ecom/utils/ObjectValidator.java
+++ src/main/java/com/example/ecom/utils/ObjectValidator.java
@@ -5,16 +5,16 @@ import java.util.Set;
 
 import javax.validation.ConstraintViolation;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 @Component
 public class ObjectValidator {
-    // @Autowired
-    // @Qualifier("validator")
-    // LocalValidatorFactoryBean validatorFactory;
-
-    LocalValidatorFactoryBean validatorFactory = new LocalValidatorFactoryBean();
+    @Autowired
+    @Qualifier("validator")
+    LocalValidatorFactoryBean validatorFactory;
 
     public <T> Map<String, String> validateRequestThenReturnMessage(Map<String, String> errorResult, T t) {
         Set<ConstraintViolation<T>> violations = validatorFactory.getValidator().validate(t);
