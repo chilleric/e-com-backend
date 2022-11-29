15372e2e408232e0481e613f80f15bf7e760e622
ericchentch <ericchen.tch@gmail.com>
2022-11-29 17:04:16 +0700

    change logic authen

--- src/main/java/com/example/ecom/inventory/user/UserInventory.java
+++ src/main/java/com/example/ecom/inventory/user/UserInventory.java
@@ -1,15 +1,17 @@
 package com.example.ecom.inventory.user;
 
 import com.example.ecom.repository.user.User;
-
 import java.util.Optional;
 
 public interface UserInventory {
-    Optional<User> findUserById(String userId);
 
-    Optional<User> findUserByEmail(String email);
+  Optional<User> findUserById(String userId);
 
-    Optional<User> findUserByPhone(String phone);
+  Optional<User> getActiveUserById(String userId);
 
-    Optional<User> findUserByUsername(String username);
+  Optional<User> findUserByEmail(String email);
+
+  Optional<User> findUserByPhone(String phone);
+
+  Optional<User> findUserByUsername(String username);
 }
